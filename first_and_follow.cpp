9f4494759d926603352b2c79c66581e9e31f277e
jangidkrishna <jangid.krishna01@gmail.com>
2019-04-11 20:40:41 +0530

    some bugs

--- first_and_follow.cpp
+++ first_and_follow.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 struct lang{
     string lhs, rhs[5], first, follow;
-    int no = 0;
+    int no;
 };
 
 int main(){
@@ -13,29 +13,28 @@ int main(){
     int n;
     cin >> n;
     lang prod[10];
-    for(int j=0;j<n;j++){
-        cout << "Enter the production : ";
-        getline(cin, temp);
+    getline(cin ,temp);
+    for(int i=0;i<n;i++){
+        cout << "Enter the production : "<< endl;
+        getline(cin ,temp);
         temp_lhs.assign(temp,0,1);
-        for(int i=0;i<n;i++){
-            if(prod[i].lhs.compare(temp_lhs)==0){
-                prod[i].rhs[prod[i].no].assign(temp,2,temp.length()-2);
-                prod[i].no++;
-            } else {
-                prod[j].lhs.assign(temp,0,1);
+        for(int j=0;j<n;j++){
+            if(prod[j].lhs.compare(temp_lhs)==0){
                 prod[j].rhs[prod[j].no].assign(temp,2,temp.length()-2);
                 prod[j].no++;
+            } else {
+                prod[i].lhs.assign(temp,0,1);
+                prod[i].rhs[0].assign(temp,2,temp.length()-2);
+                prod[i].no = 1;
             }
         }
 
     }
-    for (int i = 0; i < n; i++){
-        for (int z = 0; z < prod[i].no; z++){
-            cout << prod[i].lhs << " =>  " << prod[i].rhs[z] << endl;
-        }
-        
+
+    for(int i=0;i<n;i++){
+        cout << prod[i].no ;
     }
-    
+
     return 0;
 
 }
